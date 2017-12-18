686e28c3fae9596b7e7404468f94c1401807d3db
afikf <tkafik@gmail.com>
2017-12-18 11:27:18 +0200

    add repr

--- hw2/hw2/players/min_max_player/__init__.py
+++ hw2/hw2/players/min_max_player/__init__.py
@@ -3,6 +3,7 @@ from Reversi.consts import X_PLAYER
 from abstract import AbstractPlayer
 from utils import MiniMaxAlgorithm
 from players.better_player import Player as simplePlayer
+import abstract
 
 class Player(AbstractPlayer):
     def __init__(self, setup_time, player_color, time_per_k_turns, k):
@@ -17,6 +18,9 @@ class Player(AbstractPlayer):
         self.time_for_current_move = self.time_remaining_in_round / self.turns_remaining_in_round - 0.05
 
         self.simple = simplePlayer(setup_time, player_color, time_per_k_turns, k)
+
+    def __repr__(self):
+        return '{} {}'.format(abstract.AbstractPlayer.__repr__(self), '- min_max_player')
 
     def no_more_time(self):
         self.time_for_current_move -= (time.time() - self.clock)
