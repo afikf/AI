0cee2f770f3f9252e8935e9f967be647f2e0f807
gilikarni <gilikarni@campus.technion.ac.il>
2017-12-18 18:24:07 +0200

    all changes

--- hw2/hw2/create_opening_book.py
+++ hw2/hw2/create_opening_book.py
@@ -5,19 +5,7 @@ from Reversi.consts import *
 
 NUMBER_OF_GAMES = 70
 
-def create_gamae_book():
-    opening_book_file = open('book.gam', 'r')
-    openings = {}
-
-    for line in opening_book_file.readlines():
-        game = line[:(NUM_OF_MOVES_IN_OPENING_BOOK*3)]
-        if game in openings:
-            openings[game] += 1
-        else:
-            openings[game] = 1
-
-    best_openings = dict(sorted(openings.items(), key=operator.itemgetter(1), reverse=True)[:NUMBER_OF_GAMES])
-
+def create_file(best_openings, file_name):
     dict_of_best_moves_with_values = {}
 
     for key, value in best_openings.items():
@@ -34,9 +22,51 @@ def create_gamae_book():
 
     dict_seq_to_move = {key: value[0] for key, value in dict_of_best_moves_with_values.items()}
 
-    with open('opening_book.pkl', 'wb') as target:
-        pickle.dump(dict_seq_to_move, target, protocol=pickle.HIGHEST_PROTOCOL)
+    print(dict_seq_to_move)
 
+    with open(file_name, 'wb') as target:
+        pickle.dump(dict_seq_to_move, target, protocol=pickle.HIGHEST_PROTOCOL)
+def create_gamae_book():
+    opening_book_file = open('book.gam', 'r')
+    openings = {}
+
+    for line in opening_book_file.readlines():
+        game = line[:(NUM_OF_MOVES_IN_OPENING_BOOK*3)]
+        if game in openings:
+            openings[game] += 1
+        else:
+            openings[game] = 1
+
+    best_openings = dict(sorted(openings.items(), key=operator.itemgetter(1), reverse=True)[:NUMBER_OF_GAMES])
+
+    create_file(best_openings, 'opening_book.pkl')
+
+def update_dict(dict, line, win):
+    if line in dict:
+        dict[line] += 1 if win else -1
+    else:
+        dict[line] = 1 if win else -1
+
+def create_better_opening_book():
+    opening_book_file = open('book.gam', 'r')
+
+    first_player_dict_openings_to_wins = {}
+    second_player_dict_openings_to_wins = {}
+
+    for line in opening_book_file.readlines():
+        is_tie = line[-5] == '0' and line[-6] == '0'
+
+        if is_tie:
+            continue
+
+        update_dict(dict=first_player_dict_openings_to_wins, line=line, win=line[-7] == '+')
+        update_dict(dict=second_player_dict_openings_to_wins, line=line, win=line[-7] == '-')
+
+    best_openings = dict(sorted(first_player_dict_openings_to_wins.items(), key=operator.itemgetter(1), reverse=True)[:int(NUMBER_OF_GAMES/2)])
+    best_openings.update(dict(sorted(second_player_dict_openings_to_wins.items(), key=operator.itemgetter(1), reverse=True)[:int(NUMBER_OF_GAMES/2)]))
+
+    create_file(best_openings, 'opening_book_better.pkl')
 
 if __name__ == '__main__':
     create_gamae_book()
+    create_better_opening_book()
--- hw2/hw2/players/min_max_player/__init__.py
+++ hw2/hw2/players/min_max_player/__init__.py
@@ -2,7 +2,7 @@ import time
 from Reversi.consts import *
 from abstract import AbstractPlayer
 from utils import MiniMaxAlgorithm
-from players.better_player import Player as simplePlayer
+from players.simple_player import Player as simplePlayer
 import abstract
 
 class Player(AbstractPlayer):
@@ -37,15 +37,15 @@ class Player(AbstractPlayer):
         return True
 
     def time_for_step(self):
-        return self.split_time_equally(PRECENTAGE_OF_TIME_TO_SPLIT_EQUALY*self.time_remaining_in_round) + \
-               self.split_time_not_equally(PRECENTAGE_OF_TIME_TO_SPLIT_NOT_EQUALY*self.time_remaining_in_round) - 0.05
+        return self.split_time_equally(PERCENTAGE_OF_TIME_TO_SPLIT_EQUALY * self.time_remaining_in_round) + \
+               self.split_time_not_equally(PERCENTAGE_OF_TIME_TO_SPLIT_NOT_EQUALY * self.time_remaining_in_round) - 0.05
 
     def split_time_equally(self, time_remaining):
         return time_remaining/self.turns_remaining_in_round
 
     def split_time_not_equally(self, time_remaining):
         sum_of_remaining_turns = sum(range(self.turns_remaining_in_round + 1))
-        return time_remaining * (1-(self.turns_remaining_in_round / sum_of_remaining_turns))
+        return time_remaining * (self.turns_remaining_in_round / sum_of_remaining_turns)
 
     def get_move(self, game_state, possible_moves):
         depth = 0
@@ -56,10 +56,12 @@ class Player(AbstractPlayer):
         max_value = 0
         while not self.no_more_time():
             depth += 1
-            value, move = self.algorithm.search(game_state, depth, True)
+            [value, move, searched_all_tree] = self.algorithm.search(game_state, depth, True)
             if best_move is None or value > max_value:
                 max_value = value
                 best_move = move
+            if searched_all_tree:
+                break
 
         if self.turns_remaining_in_round == 1:
             self.turns_remaining_in_round = self.k
