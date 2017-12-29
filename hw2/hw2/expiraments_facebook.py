881c322762d31cbe78f94d29e8a7f56b95029667
gilikarni <gilikarni@campus.technion.ac.il>
2017-12-29 16:12:46 +0200

    New expiraments

--- hw2/hw2/expiraments_facebook.py
+++ hw2/hw2/expiraments_facebook.py
@@ -6,11 +6,12 @@ import os
 import shutil
 import time
 
-num_of_cores = 4
+num_of_cores = 1
 sema = threading.Semaphore(value=num_of_cores)
 
 players = ['simple_player', 'alpha_beta_player', 'min_max_player', 'better_player']
 times = ['2', '10', '50']
+exceeded = {'simple_player': 0, 'alpha_beta_player': 0, 'min_max_player': 0, 'better_player': 0}
 
 def callto(time, p1, p2):
 
@@ -21,8 +22,8 @@ def callto(time, p1, p2):
     file = open(file_name, 'w+')
 
     for _ in range(5):
-        print('python3 run_game.py 2 {} 5 n {} {}'.format(time, p1, p2))
-        call(['python3', 'run_game.py', '2', time, '5', 'n', p1, p2], stdout=file)
+        print('run_game.py 2 {} 5 n {} {}'.format(time, p1, p2))
+        call(['run_game.py', '2', time, '5', 'n', p1, p2], stdout=file, shell=True)
 
     file.close()
 
@@ -46,8 +47,9 @@ def run_threads():
 
 
 def create_fianl_reult_and_csv_file():
+
     final_result = {player: {t: 0 for t in times} for player in players}
-    final = open('final.csv', 'w')
+    final = open('final_sync.csv', 'w')
     for p1 in players:
         for p2 in players:
             if p1 == p2:
@@ -56,11 +58,14 @@ def create_fianl_reult_and_csv_file():
                 file_name = 'temp/' + p1 + p2+time+'.txt'
                 with open(file_name, 'r') as file:
                     for line in file.readlines():
-                        print('line is:{}'.format(line))
                         winner = re.split('\n', line)[0].split(' ')[-1]
                         if winner == 'resources.':
+                            player = re.split('\n', line)[0].split(' ')[1]
+                            if player == 'X':
+                                exceeded[p1] += 1
+                            else:
+                                exceeded[p2] += 1
                             continue
-                        winner += '_player'
                         p1_score = '0.5'
                         p2_score = '0.5'
                         if winner == p1:
@@ -79,7 +84,7 @@ def create_fianl_reult_and_csv_file():
 
 
 def create_graph_and_final_table(final_result):
-    final_table = open('final_table.csv', 'w')
+    final_table = open('final_table_sync.csv', 'w')
     headers = 't = 2, t = 10, t = 50, player_name\n'
     final_table.write(headers)
     plt.figure()
@@ -110,6 +115,9 @@ def main():
 
     if os.path.isdir('temp'):
         shutil.rmtree('temp')
+
+    for p in players:
+        print("{} exceeded time {} times".format(p, exceeded[p]))
 
 
 if __name__ == '__main__':
