3b80f1dfe28f297ebe9d11ae54d12c1a668429aa
afikf <tkafik@gmail.com>
2018-01-15 18:01:28 +0200

    q1 with graph and diagram

--- AI3/q1.py
+++ AI3/q1.py
@@ -5,6 +5,42 @@ from sklearn.model_selection import cross_val_score, cross_val_predict
 from sklearn.metrics import confusion_matrix
 import matplotlib.pyplot as plt; plt.rcdefaults()
 import matplotlib.pyplot as plt
+import itertools
+
+def plot_confusion_matrix(cm, classes,
+                          normalize=False,
+                          title='Confusion matrix',
+                          cmap=plt.cm.Blues):
+    """
+    This function prints and plots the confusion matrix.
+    Normalization can be applied by setting `normalize=True`.
+    """
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+        print("Normalized confusion matrix")
+    else:
+        print('Confusion matrix, without normalization')
+
+    print(cm)
+
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes, rotation=45)
+    plt.yticks(tick_marks, classes)
+
+    fmt = '.2f' if normalize else 'd'
+    thresh = cm.max() / 2.
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, format(cm[i, j], fmt),
+                 horizontalalignment="center",
+                 color="white" if cm[i, j] > thresh else "black")
+
+    plt.tight_layout()
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
+
 
 func = lambda x: 0.0 if x == b'False' else 1.0
 all_data = loadtxt('flare.csv', delimiter=',', skiprows=1, converters={32: func})
@@ -21,9 +57,18 @@ results = (conf_mat.reshape((1, 4))).tolist()[0]
 plt.bar(y_pos, results, align='center', alpha=0.5)
 plt.xticks(y_pos, labels)
 plt.title('Confusion Matrix')
-plt.show()
 print(succ_rate_avg)
 print(conf_mat)
+plt.figure()
+class_names = ['True', 'False']
+plot_confusion_matrix(conf_mat, classes=class_names,
+                      title='Confusion matrix, without normalization')
+plt.show()
+
+a=DecisionTreeClassifier()
+a.fit()
+
+
 
 
 
