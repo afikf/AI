dfb4da60a46c18cd5a357557e3350a7ba3e65d89
afikf <tkafik@gmail.com>
2018-01-15 16:48:10 +0200

    q1 with graph

--- AI3/q1.py
+++ AI3/q1.py
@@ -3,6 +3,8 @@ from numpy import loadtxt
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.model_selection import cross_val_score, cross_val_predict
 from sklearn.metrics import confusion_matrix
+import matplotlib.pyplot as plt; plt.rcdefaults()
+import matplotlib.pyplot as plt
 
 func = lambda x: 0.0 if x == b'False' else 1.0
 all_data = loadtxt('flare.csv', delimiter=',', skiprows=1, converters={32: func})
@@ -13,6 +15,13 @@ succ_rate = cross_val_score(clf, data, target, cv=4)
 target_predict = cross_val_predict(clf, data, target, cv=4)
 succ_rate_avg = np.average(succ_rate)
 conf_mat = confusion_matrix(target, target_predict)
+labels = ('True Negative', 'False Positive', 'False Negative', 'True Positive')
+y_pos = np.arange(len(labels))
+results = (conf_mat.reshape((1, 4))).tolist()[0]
+plt.bar(y_pos, results, align='center', alpha=0.5)
+plt.xticks(y_pos, labels)
+plt.title('Confusion Matrix')
+plt.show()
 print(succ_rate_avg)
 print(conf_mat)
 
