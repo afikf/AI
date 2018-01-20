c39b004cf985f0ffd76287fb261dbb61ed98f037
gilikarni <gilikarni@campus.technion.ac.il>
2018-01-20 12:19:10 +0200

    implement sys

--- AI3/sfs.py
+++ AI3/sfs.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 def sfs(x, y, k, clf, score):
     """
     :param x: feature set to be trained using clf. list of lists.
@@ -7,4 +9,24 @@ def sfs(x, y, k, clf, score):
     :param score: utility function for the algorithm, that receives clf, feature subset and labeles, returns a score. 
     :return: list of chosen feature indexes
     """
-    raise NotImplementedError
+    if k == 0:
+        return []
+
+    indices = sfs(x, y, k - 1, clf, score)
+
+    max_utility = None
+    max_index = 0
+
+    for index in range(len(x[0])):
+        if index in indices:
+            continue
+
+        samples = np.asarray(x)
+        samples = samples[:, indices + [index]]
+        utility = score(clf, samples, y)
+
+        if utility > max_utility:
+            max_utility = utility
+            max_index = index
+
+    return indices + [max_index]
